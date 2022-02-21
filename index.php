081f62bcc37142a2f38f1e8f33ce60e1e9881da8
Vedashri Waman <60397244+VEDdroid@users.noreply.github.com>
2022-02-21 21:07:54 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,102 +1,87 @@
+<?php 
+$user = $conn->query("SELECT * FROM `accounts` where id ='".$_settings->userdata('id')."'");
+foreach($user->fetch_array() as $k =>$v){
+	$meta[$k] = $v;
+}
+?>
 <?php if($_settings->chk_flashdata('success')): ?>
 <script>
 	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
 <?php endif;?>
 <div class="card card-outline card-primary">
-	<div class="card-header">
-		<h3 class="card-title">List of Announcements</h3>
-		<div class="card-tools">
-			<a href="?page=announcements/manage_announcement" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
-		</div>
-	</div>
 	<div class="card-body">
-        <div class="container-fluid">
-			<table class="table table-bordered table-stripped" id="indi-list">
-				<colgroup>
-					<col width="5%">
-					<col width="15%">
-					<col width="20%">
-					<col width="35%">
-					<col width="15%">
-					<col width="10%">
-				</colgroup>
-				<thead>
-					<tr>
-						<th class="text-center">#</th>
-						<th>Date Created</th>
-						<th>Title</th>
-						<th>Body</th>
-						<th>Date Last Update</th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php 
-					$i = 1;
-						$qry = $conn->query("SELECT * from `announcements` order by unix_timestamp(date_created) desc ");
-						while($row = $qry->fetch_assoc()):
-							$row['announcement'] = strip_tags(stripslashes(html_entity_decode($row['announcement'])))
-					?>
-					
-						<tr>
-							<td class="text-center"><?php echo $i++; ?></td>
-							<td><?php echo $row['date_created'] ?></td>
-							<td><?php echo $row['title'] ?></td>
-							<td><p class="m-o truncate"><?php echo $row['announcement'] ?></p></td>
-							<td><?php echo $row['date_updated'] ?></td>
-							<td align="center">
-								 <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-				                  		Action
-				                    <span class="sr-only">Toggle Dropdown</span>
-				                  </button>
-				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item edit_data" href="./?page=announcements/manage_announcement&id=<?php echo $row['id'] ?>" data-id="<?php echo $row['id'] ?>"> Edit</a>
-                                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"> Delete</a>
-				                  </div>
-							</td>
-						</tr>
-					<?php endwhile; ?>
-				</tbody>
-			</table>
+		<div class="container-fluid">
+			<div id="msg"></div>
+			<form action="" id="manage-user">	
+				<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
+				<div class="form-group">
+					<label for="name">First Name</label>
+					<input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo isset($meta['firstname']) ? $meta['firstname']: '' ?>" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Last Name</label>
+					<input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo isset($meta['lastname']) ? $meta['lastname']: '' ?>" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" name="email" id="email" class="form-control" value="<?php echo isset($meta['email']) ? $meta['email']: '' ?>" required  autocomplete="off">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
+					<small><i>Leave this blank if you dont want to change the password.</i></small>
+				</div>
+			</form>
 		</div>
 	</div>
+	<div class="card-footer">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-sm btn-primary" form="manage-user">Update</button>
+				</div>
+			</div>
+		</div>
 </div>
+<style>
+	img#cimg{
+		height: 15vh;
+		width: 15vh;
+		object-fit: cover;
+		border-radius: 100% 100%;
+	}
+</style>
 <script>
-	var indiList;
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this Announcement?","delete_announcement",[$(this).attr('data-id')])
-		})
-	})
-	function delete_announcement($id){
-		start_loader();
+	function displayImg(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
+	$('#manage-user').submit(function(e){
+		e.preventDefault();
+		start_loader()
 		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_announcement",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
-				console.log(err)
-				alert_toast("An error occured.",'error');
-				end_loader();
-			},
+			url:_base_url_+'classes/Users.php?f=save_client',
+			data: new FormData($(this)[0]),
+		    cache: false,
+		    contentType: false,
+		    processData: false,
+		    method: 'POST',
+		    type: 'POST',
 			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
-					location.reload();
+				if(resp ==1){
+					location.reload()
 				}else{
-					alert_toast("An error occured.",'error');
-					end_loader();
+					$('#msg').html('<div class="alert alert-danger">Username already exist</div>')
+					end_loader()
 				}
 			}
 		})
-	}
-	$(function(){
-		indiList = $('#indi-list').dataTable({
-			columnDefs:[{
-				targets:[5],
-				orderable:false
-			}],
-		});
 	})
+
 </script>
